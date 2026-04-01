7bbb66c2891c417c4546d2cd6d6e0c4526cd1b4f
F. Paiva <fpaiva1011@gmail.com>
2026-04-01 10:01:47 +0100

    Changed harpoon to nonconflicting keybindings

--- init.lua
+++ init.lua
@@ -207,10 +207,10 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+-- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+-- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+-- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
@@ -906,12 +906,12 @@ require('lazy').setup({
       -- REQUIRED
 
       vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end, { desc = 'Add to Harpoon' })
-      vim.keymap.set('n', '<S-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+      vim.keymap.set('n', '<C-S-E>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-      vim.keymap.set('n', '<S-h>', function() harpoon:list():select(1) end)
-      vim.keymap.set('n', '<S-j>', function() harpoon:list():select(2) end)
-      vim.keymap.set('n', '<S-k>', function() harpoon:list():select(3) end)
-      vim.keymap.set('n', '<S-l>', function() harpoon:list():select(4) end)
+      vim.keymap.set('n', '<C-S-H>', function() harpoon:list():select(1) end)
+      vim.keymap.set('n', '<C-S-J>', function() harpoon:list():select(2) end)
+      vim.keymap.set('n', '<C-S-K>', function() harpoon:list():select(3) end)
+      vim.keymap.set('n', '<C-S-L>', function() harpoon:list():select(4) end)
 
       -- Toggle previous & next buffers stored within Harpoon list
       vim.keymap.set('n', '<C-S-P>', function() harpoon:list():prev() end)
